6fc1cf31eade9c5a823856576ea7bc21a0bc5d66
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-01-05 17:29:51 -0300

    Bump deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.majorcluster/pedestal-api-helper "0.5.1"
+(defproject org.clojars.majorcluster/pedestal-api-helper "0.6.1"
   :description "Useful simple tools to be used for pedestal APIs"
   :url "https://github.com/mtsbarbosa/pedestal-api-helper"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]]
   :source-paths ["src/clj"]
-  :profiles {:dev {:dependencies [[nubank/matcher-combinators "1.2.1"]]}})
+  :profiles {:dev {:dependencies [[nubank/matcher-combinators "3.7.2"]]}})
