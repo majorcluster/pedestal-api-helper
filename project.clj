03a732ab14f19d6bda0e2249a09b4a0f1c2a9b40
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:38:26 -0300

    Adjusting license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.majorcluster/pedestal-api-helper "0.8.0"
+(defproject org.clojars.majorcluster/pedestal-api-helper "0.8.1"
   :description "Useful simple tools to be used for pedestal APIs"
-  :url "https://github.com/mtsbarbosa/pedestal-api-helper"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/majorcluster/pedestal-api-helper"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]]
   :source-paths ["src/clj"]
