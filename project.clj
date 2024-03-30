e3f79cbf6df3493c5c79bb3487ba004b171f5968
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2024-03-30 12:17:55 -0300

    bumping libraries

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.majorcluster/pedestal-api-helper "0.11.1"
+(defproject org.clojars.majorcluster/pedestal-api-helper "0.11.0"
   :description "Useful simple tools to be used for pedestal APIs"
   :url "https://github.com/majorcluster/pedestal-api-helper"
   :license {:name "The MIT License"
